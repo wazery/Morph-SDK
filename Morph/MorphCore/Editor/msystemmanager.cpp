9b0451ea8edab0817f8498c8347d31ce41ab54ea
Islam Wazery <wazery@ubuntu.com>
2012-02-14 01:36:42 +0200

    Added the empty system manager class

---
+++ Morph/MorphCore/Editor/msystemmanager.cpp
@@ -0,0 +1,5 @@
+#include "msystemmanager.h"
+
+MSystemManager::MSystemManager()
+{
+}
---
+++ Morph/MorphCore/Editor/msystemmanager.h
@@ -0,0 +1,10 @@
+#ifndef MSYSTEMMANAGER_H
+#define MSYSTEMMANAGER_H
+
+class MSystemManager
+{
+public:
+    MSystemManager();
+};
+
+#endif // MSYSTEMMANAGER_H
