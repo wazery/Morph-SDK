201e19c8bc8a6a1be1ca3922d2148865f3775a7a
Islam Wazery <wazery@ubuntu.com>
2012-02-14 05:53:26 +0200

    Added interface for the System Manager

--- Morph/MorphCore/Editor/msystemmanager.cpp
+++ Morph/MorphCore/Editor/msystemmanager.cpp
@@ -1,5 +1,120 @@
-#include "msystemmanager.h"
+/* ============================================================
+ *
+ * This file is a part of Morph project
+ *
+ * Date        : 2011-10-26
+ * @file msystemmanager.cpp
+ * @brief A manager which is responsible for creating the basic things
+ * that Ogre needs to start, in another word it bootstraps Ogre.
+ *
+ * Copyright (C) 2011-2012 by @author Islam Wazery <wazery at ubuntu dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "MSystemManager.h"
+
+// Total number of lines in one dimension will be (GRID_LINE_NUM * 2 + 1)
+#define GRID_LINE_NUM 3
+
+// Units of space between lines
+#define GRID_LINE_SPACE 80
 
 MSystemManager::MSystemManager()
+{
+}
+
+MSystemManager::~MSystemManager(void)
+{
+}
+
+bool MSystemManager::initialise(String windowName, int width, int height, NameValuePairList *parms, Root *pRoot)
+{
+	/** @return name */
+}
+
+void MSystemManager::shutDown()
+{
+}
+
+void MSystemManager::doRender()
+{
+}
+
+void MSystemManager::createGrid()
+{
+}
+
+void MSystemManager::resizeWindow(int width, int height)
+{
+}
+
+bool MSystemManager::initOgreCore(String windowName, int width, int height, Ogre::NameValuePairList *parms)
+{
+	/** @return name */
+}
+
+void MSystemManager::createScene()
+{
+}
+
+void MSystemManager::destroyScene()
+{
+}
+
+void MSystemManager::createResourceListener()
+{
+}
+
+void MSystemManager::createFrameListener()
+{
+}
+
+void MSystemManager::addResourceLocations()
+{
+}
+
+void MSystemManager::initResources()
+{
+}
+
+void MSystemManager::updateStats(void)
+{
+}
+
+bool MSystemManager::frameStarted(const FrameEvent& evt)
+{
+	/** @return name */
+}
+
+bool MSystemManager::frameEnded(const FrameEvent& evt)
+{
+}
+
+void MSystemManager::createGridObjects()
+{
+}
+
+ MSystemManager* MSystemManager::getSingletonPtr()
+{
+	 /** @return name */
+}
+
+ MSystemManager& MSystemManager::getSingleton()
+{
+	 /** @return name */
+}
+
+ void MSystemManager::releaseSingleton()
 {
 }
--- Morph/MorphCore/Editor/msystemmanager.h
+++ Morph/MorphCore/Editor/msystemmanager.h
@@ -1,10 +1,180 @@
+/* ============================================================
+ *
+ * This file is a part of Morph project
+ *
+ * Date        : 2011-10-26
+ * @file msystemmanager.cpp
+ * @brief A manager which is responsible for creating the basic things
+ * that Ogre needs to start, in another word it bootstraps Ogre.
+ *
+ * Copyright (C) 2011-2012 by @author Islam Wazery <wazery at ubuntu dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
 #ifndef MSYSTEMMANAGER_H
 #define MSYSTEMMANAGER_H
 
-class MSystemManager
+#include <QtGui>
+
+// FIXME: Use Ogre singleton implementation untill
+// implementing one for the editor core
+//#include <OgreSingleton.h>
+#include <OGRE/Ogre.h>
+
+//#include "MSingleton.h" // FIXME: Implement this header file.
+
+using namespace Ogre; // FIXME: Remove this line. What makes me need it is the Frame listner inheritnce!
+// TODO: Create a namespace Morph-Editor, for the editor sub-systems
+
+/** @class*/
+class MSystemManager : public FrameListener, public QWidget
 {
 public:
-    MSystemManager();
+    /** Default constructor */
+    MSystemManager(void);
+
+    /** Finaliser */
+    ~MSystemManager(void);
+
+    // FIXME: retest this overridden function.
+    // Override QWidget::paintEngine to return NULL
+    // Turn off Qts paint engine for the Ogre widget.
+    //QPaintEngine *paintEngine() const;
+
+    /** Initialise Ogre System
+        @remarks
+            Create a render window. This window should be the current
+            ChildView window using the externalWindowHandle value pair option.
+        @param windowName QString identifying the name of the parent window.
+        @param width int containing the width of the Ogre Canvas.
+        @param height int containing the height of the Ogre Canvas.
+        @param params NameValuePairList Name / value parameter pair (first = name, second = value).
+        @param pRoot Root pointer to the Ogre root.
+    */
+    bool initialise(String windowName, int width, int height, NameValuePairList *params, Root *pRoot=NULL);
+
+    /** Rendring the canvas */
+    void doRender();
+
+    /** Create the grid for the canvas */
+    void createGrid();
+
+    /** change window size */
+    void resizeWindow(int width, int height);
+
+    /** Access the Root */
+    Ogre::Root* getRoot(void) const    { /** @return mRoot */ return(mRoot); }
+
+    /** Access the SceneManager */
+    Ogre::SceneManager* getSceneManager(void) const    { /** @return mSceneManager */ return(mSceneManager); }
+
+    /** Access the MainCamera */
+    Ogre::Camera* getMainCamera(void) const    { /** @return mMainCamera */ return(mMainCamera); }
+
+    /** Access the CurrentCamera */
+    Ogre::Camera* getCurrCamera(void) const    { /** @return mCurrCamera */ return(mCurrCamera); }
+
+    /** Access the mIsInitialised */
+    bool isInitialised(void) const    { /** @return mIsInitialised */ return(mIsInitialised);}
+
+    /** Access the RenderWindow */
+    Ogre::RenderWindow* getRenderWindow() const    { /** @return mRenderWindow */ return(mRenderWindow);}
+
+    /** Access the WindowWidth */
+    int getWindowWidth(void) const    { /** @return mWindowWidth */ return(mWindowWidth); }
+
+    /** Access the WindowHeight */
+    int getWindowHeight(void) const    { /** @return mWindowHeight */ return(mWindowHeight); }
+
+    /** Sets the viewport background */
+    void setBackgroundColour(QColor c);
+
+    /** Sets the current camera position */
+    void setCameraPosition(const Ogre::Vector3 &pos);
+
+protected:
+     /** Initialise Ogre Core
+        @remarks
+            Create a render window. This window should be the current
+            childView window using the externalWindowHandle value pair option.
+        @param windowName QString identifying the name of the parent window.
+        @param width int containing the width of the Ogre Canvas.
+        @param height int containing the height of the Ogre Canvas.
+        @param params NameValuePairList Name / value parameter pair (first = name, second = value).
+    */
+    bool initOgreCore(String windowName, int width, int height, Ogre::NameValuePairList *params);
+
+    /** Shut Down Ogre System */
+    void shutDown();
+
+    /** Creates the entire scene
+        @remarks
+            It can be overriden, as it is a pure virtual.
+    */
+    void createScene();
+
+    /** Destroys the entire scene */
+    void destroyScene();
+
+    // FIXME: This function has no implementation. May be later implemented for the loading
+    // resources progress bar in the editor view
+    //void createResourceListener();
+    void createFrameListener();
+
+    /** Setup paths to all resources */
+    void addResourceLocations();
+
+    /** Setup the resources for Ogre */
+    void initResources();
+
+    void updateStats(void);
+
+    // FrameListener overrides
+    virtual bool frameStarted(const FrameEvent& evt);
+    virtual bool frameEnded(const FrameEvent& evt);   
+
+    // TODO: check if this function is logical
+    void addViewport(Ogre::Camera camera);
+
+public:
+    static MSystemManager* smInstance;
+
+    /** Get the class address pointer */
+    static MSystemManager* getSingletonPtr();
+
+    static MSystemManager &getSingleton();
+
+    static void releaseSingleton();
+
+protected:
+    Ogre::Root          *mRoot;
+    Ogre::SceneManager  *mSceneManager;
+    Ogre::RenderWindow  *mRenderWindow;
+    Ogre::Viewport      *mViewport;
+    Ogre::Overlay       *mDebugOverlay; // TODO: change its name
+
+    Ogre::ManualObject  *mFloorGrid;
+    Ogre::ManualObject  *mCircle;
+
+    Ogre::Camera        *mMainCamera;
+    Ogre::Camera        *mCurrCamera;
+
+    bool		mIsInitialised;
+    int			mWindowWidth;
+    int			mWindowHeight;
 };
+
+MSystemManager* MSystemManager::smInstance;  // FIXME: Partially understood.
 
 #endif // MSYSTEMMANAGER_H
