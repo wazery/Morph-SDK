86e9649b007a6e38a2efcd988d5fab62e4b60d58
Islam Wazery <wazery@ubuntu.com>
2012-02-18 10:18:52 +0200

    Implemented the log manager

---
+++ Morph/MorphCore/Editor/mlogmanager.cpp
@@ -0,0 +1,82 @@
+/* ============================================================
+ *
+ * This file is a part of Morph project
+ *
+ * Date        : 2011-10-26
+ * @file mlogmanager.h
+ * @brief A manager which is responsible for logging Morph messages
+ * to the user, its implementation is similar to the Ogre log implementation.
+ *
+ * Copyright (C) 2011-2012 by @author Islam Wazery <wazery at ubuntu dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "MLogManager.h"
+#include <iomanip>
+
+MLogManager::MLogManager()
+{
+    mOutputFile.open("Morph_log.log", std::ios::out);
+}
+
+MLogManager::~MLogManager()
+{
+    mOutputFile.close();
+}
+
+void MLogManager::logOutput(const std::string &message, logType type, bool writeToFile)
+{
+    for (std::vector<MLogListener*>::iterator it = mLogListnerList.begin(); it != mLogListnerList.end(); ++it)
+        (*it)->messageLogged(message, type);
+
+	if (writeToFile && mOutputFile.is_open())
+	{
+		struct tm *pTime;
+		time_t ctTime;
+        time(&ctTime);
+		pTime = localtime(&ctTime);
+		mOutputFile << std::setw(2) << std::setfill(('0')) << pTime->tm_hour
+			<< (":") << std::setw(2) << std::setfill(('0')) << pTime->tm_min
+			<< (":") << std::setw(2) << std::setfill(('0')) << pTime->tm_sec
+			<< (":") << message << std::endl;
+
+		// Flush stcmdream to ensure it is written (incase of a crash, we need log to be up to date)
+		mOutputFile.flush();
+	}
+}
+
+MLogManager& MLogManager::getSingleton()
+{
+    if (smInstance == NULL)
+        smInstance = new MLogManager();
+
+    return *smInstance;
+}
+
+MLogManager* MLogManager::getSingletonPtr()
+{
+    if (smInstance = NULL)
+        smInstance = new MLogManager();
+
+    return smInstance;
+}
+
+void MLogManager::releaseSingleton()
+{
+    if (smInstance)
+    {
+        delete smInstance;
+        smInstance = NULL;
+    }
+}
